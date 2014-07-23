5f66e6f8815ad1139897ef941092b4cf5d479441
benbenye <1203846281@qq.com>
2014-07-23 23:07:57 +0800

    init

---
+++ Spider/Downloader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Spider
+{
+    class Downloader : IDownloader
+    {
+        
+        public Downloader()
+        {
+        }
+        public void Download(string url)
+        {
+            HttpWebRequest hwr = WebRequest.CreateHttp()
+        }
+    }
+}
