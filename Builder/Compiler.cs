b182b258aa4c578e039d5dacc13f51d72da9b4ae
wmzy <1256573276@qq.com>
2014-08-08 18:42:33 +0800

    开始处理编译，html下载还有一点问题，导致编译异常退出

---
+++ Builder/Compiler.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Builder
+{
+    public class Compiler
+    {
+        public string FileName { get; set; }
+        public string CompileInfo { get; private set; }
+        public string ProgreassInfo { get; private set; }
+
+        delegate bool GetInfoCall(string info);
+
+        //编译信息
+        public bool GetCompileInfoCall(string info)
+        {
+            CompileInfo = info;
+            return true;
+        }
+
+        //进度信息
+        public bool GetProgreassInfoCall(string info)
+        {
+            ProgreassInfo = info;
+            return true;
+        }
+
+        [DllImport("hha.dll", EntryPoint = "HHA_CompileHHP")]
+        private extern static void CompileHHP(string hhpFile, GetInfoCall getCompileInfoCall, GetInfoCall getProgreassInfoCall, int stack);
+
+        public void Compile()
+        {
+            CompileHHP(FileName, GetCompileInfoCall, GetProgreassInfoCall, 0);
+        }
+    }
+}
