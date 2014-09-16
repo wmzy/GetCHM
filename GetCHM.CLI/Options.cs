5911c49edffd4bafff106bf319f695008adbba94
wmzy <1256573276@qq.com>
2014-09-16 18:46:40 +0800

    add options

---
+++ GetCHM.CLI/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace GetCHM.CLI
+{
+    class Options
+    {
+        [OptionArray('s', "seed", Required = true, HelpText = "种子url")]
+        public string[] SeedUrls { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            // this without using CommandLine.Text
+            //  or using HelpText.AutoBuild
+            var usage = new StringBuilder();
+            usage.AppendLine("GetCHM-CLI 1.0");
+            usage.AppendLine("Read user manual for usage instructions...");
+            return usage.ToString();
+        }
+    }
+}
--- GetCHM.CLI/Program.cs
+++ GetCHM.CLI/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace GetCHM.CLI
+﻿using System;
+
+namespace GetCHM.CLI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // consume Options instance properties
+            }
         }
     }
 }
